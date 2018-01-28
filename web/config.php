8a6fe7609c8d9822020065fa668d6edd49eaf1a5
xcwl <320250350@QQ.COM>
2018-01-28 22:45:32 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyAbJnpMWBZ5RZN1qy42-SqVD4hwzYXXQcg');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
